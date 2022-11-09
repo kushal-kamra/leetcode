223e48ca8229c05b112b48defa7f7301ddf2854c
kushal-kamra <kushalkamradev@gmail.com>
2022-11-09 14:04:31 +0000

    Adding solution for 61 in golang

---
+++ 61. Rotate List/61.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ package program
+
+ type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+ func rotateRight(head *ListNode, k int) *ListNode {
+    if head == nil || head.Next == nil {
+		return head
+	}
+
+	old_tail := head
+	n := 0
+
+	for n = 1; old_tail.Next != nil; n++ {
+		old_tail = old_tail.Next
+	}
+
+	old_tail.Next = head
+	new_tail := head
+
+	for i := 0; i < n-k%n-1; i++ {
+		new_tail = new_tail.Next
+	}
+
+	new_head := new_tail.Next
+	new_tail.Next = nil
+
+	return new_head
+}
