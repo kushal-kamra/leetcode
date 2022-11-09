75cca853e2e617d5b0629f2b70d69c12591949f5
kushal-kamra <kushalkamradev@gmail.com>
2022-11-09 06:26:53 +0000

    Added solution for 138 in Golang

---
+++ 138. Copy List with Random Pointer/138.go
@@ -0,0 +1,51 @@
+//138. Copy List with Random Pointer solution in Golang
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+package program
+
+type Node struct {
+	Val int
+	Next *Node
+	Random *Node
+}
+
+func copyRandomList(head *Node) *Node {
+    if head == nil {
+        return head
+    }
+
+    var ptr = head
+    var new_ptr_map = make(map[*Node]*Node)
+
+    for ptr != nil {
+        new_ptr_map[ptr] = &Node{Val: ptr.Val}
+        ptr = ptr.Next
+    }
+
+    ptr = head
+
+    for ptr != nil {
+        if new_ptr_map[ptr.Next] != nil {
+            new_ptr_map[ptr].Next = new_ptr_map[ptr.Next]
+        } else {
+            new_ptr_map[ptr].Next = nil
+        }
+
+        if new_ptr_map[ptr.Random] != nil {
+            new_ptr_map[ptr].Random = new_ptr_map[ptr.Random]
+        } else {
+            new_ptr_map[ptr].Random = nil
+        }
+
+        ptr = ptr.Next
+    }
+
+    return new_ptr_map[head]
+}
