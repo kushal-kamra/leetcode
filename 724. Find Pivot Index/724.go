993b299739a2b1997da630785bafeb7f86e6efcc
kushal-kamra <kushalkamradev@gmail.com>
2022-11-10 04:32:34 +0000

    Adding solution to 724 in Golang

---
+++ 724. Find Pivot Index/724.go
@@ -0,0 +1,22 @@
+//724. Find Pivot Index solution in Golang
+//https://leetcode.com/problems/find-pivot-index/description/
+
+package leetcode
+
+func pivotIndex(nums []int) int {
+    sum, leftSum, length := 0, 0, len(nums)
+
+    for n := 0; n < length; n++ {
+        sum = sum + nums[n]
+    }
+
+    for n:= 0; n < length; n++ {
+        if leftSum == sum - leftSum - nums[n] {
+            return n
+        }
+
+        leftSum = leftSum + nums[n]
+    }
+
+    return -1
+}
