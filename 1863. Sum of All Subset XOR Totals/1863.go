07d43b316369def6c799eb84f00d0918cebf6d72
kushal-kamra <kushalkamradev@gmail.com>
2022-11-18 09:30:16 +0000

    Adding solution for 1863. Sum of All Subset XOR Totals solution in GoLang

---
+++ 1863. Sum of All Subset XOR Totals/1863.go
@@ -0,0 +1,19 @@
+// 1863. Sum of All Subset XOR Totals solution in GoLang
+// https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/
+
+package leetcode
+
+func subsetXORSum(nums []int) int {
+    return helper(nums, 0, 0)
+}
+
+func helper(nums []int, index, current_xor int) int {
+    if index == len(nums) {
+        return current_xor
+    }
+
+    with_element := helper(nums, index + 1, current_xor ^ nums[index])
+    without_element := helper(nums, index + 1, current_xor)
+
+    return with_element + without_element
+}
