d384c557665aa49b8b06ba055df950af3b26db27
kushal-kamra <kushalkamradev@gmail.com>
2022-11-28 13:16:52 +0000

    Adding 14. Longest Common Prefix solution in GoLang

---
+++ 14. Longest Common Prefix/14.go
@@ -0,0 +1,18 @@
+// 14. Longest Common Prefix solution in GoLang
+// https://leetcode.com/problems/longest-common-prefix/description/
+
+package leetcode
+
+func longestCommonPrefix(strs []string) string {
+    p := strs[0]
+
+    for _, s := range strs {
+        i := 0
+
+        for ; i < len(s) && i < len(p) && p[i] == s[i]; i++ {}
+
+        p = p[:i]
+    }
+
+    return p
+}
