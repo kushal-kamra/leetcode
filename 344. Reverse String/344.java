52143d84256b4ffd24318d47b7a74661d40cf14e
kushal-kamra <kushalkamradev@gmail.com>
2022-11-28 13:57:33 +0000

    Adding 344. Reverse String solution in Java

---
+++ 344. Reverse String/344.java
@@ -0,0 +1,17 @@
+// 344. Reverse String solution in Java
+// https://leetcode.com/problems/reverse-string/description/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
