1fb0f210d780a79b0a7c5d394b4d58633ee9bfd0
kushal-kamra <kushalkamradev@gmail.com>
2022-11-28 13:09:12 +0000

    Adding 14. Longest Common Prefix solution in JavaScript

---
+++ 14. Longest Common Prefix/14.js
@@ -0,0 +1,18 @@
+// 14. Longest Common Prefix solution in JavaScript
+// https://leetcode.com/problems/longest-common-prefix/description/
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    if (strs == undefined || strs.length == 0) return "";
+
+    return strs.reduce((prev, next) => {
+        let i = 0;
+
+        while (prev[i] && next[i] && prev[i] == next[i]) i++;
+
+        return prev.slice(0, i);
+    });
+};
