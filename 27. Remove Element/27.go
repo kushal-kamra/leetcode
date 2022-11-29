a9d2d0a1eb0e276a4c212441ce8ff7aca7ae7f4f
kushal-kamra <kushalkamradev@gmail.com>
2022-11-29 03:03:44 +0000

    Adding 27. Remove Element solution in GoLang

---
+++ 27. Remove Element/27.go
@@ -0,0 +1,18 @@
+// 27. Remove Element solution in GoLang
+// https://leetcode.com/problems/remove-element/description/
+
+package leetcode
+
+func removeElement(nums []int, val int) int {
+    length := len(nums)
+    i := 0
+
+    for j := 0; j < length; j++ {
+        if nums[j] != val {
+            nums[i] = nums[j]
+            i++
+        }
+    }
+
+    return i
+}
