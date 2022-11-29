36df19593003ccdffa85c256c87ebb698aec7154
kushal-kamra <kushalkamradev@gmail.com>
2022-11-29 07:45:42 +0000

    Adding 283. Move Zeroes solution in GoLang

---
+++ 283. Move Zeroes/283.go
@@ -0,0 +1,19 @@
+// 283. Move Zeroes solution in GoLang
+// https://leetcode.com/problems/move-zeroes/description/
+
+package leetcode
+
+func moveZeroes(nums []int)  {
+    last_non_zero := 0
+
+    for i := 0; i < len(nums); i++ {
+        if nums[i] != 0 {
+            nums[last_non_zero] = nums[i]
+            last_non_zero++
+        }
+    }
+
+    for i := last_non_zero; i < len(nums); i++ {
+        nums[i] = 0
+    }
+}
