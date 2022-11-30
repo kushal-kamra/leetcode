b218b3948a43377a140062d9188e803bc0b074ca
kushal-kamra <kushalkamradev@gmail.com>
2022-11-30 12:30:06 +0000

    Adding 94. Binary Tree Inorder Traversal recursive solution in GoLang

---
+++ 94. Binary Tree Inorder Traversal/94-recursive.go
@@ -0,0 +1,32 @@
+// 94. Binary Tree Inorder Traversal recursive solution in GoLang
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+package leetcode
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+ 
+func inorderTraversal(root *TreeNode) []int {
+	if root == nil {
+        return nil
+    }
+
+    var output []int
+    traverse(root, &output)
+
+    return output
+}
+
+func traverse(root *TreeNode, output *[]int) {
+    if root == nil {
+        return
+    }
+
+    traverse(root.Left, output)
+    *output = append(*output, root.Val)
+    traverse(root.Right, output)
+}
