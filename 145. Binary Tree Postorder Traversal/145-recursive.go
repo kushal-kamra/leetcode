20e09ddf39627f1e2856542648255cf7ccfa845c
kushal-kamra <kushalkamradev@gmail.com>
2022-12-05 14:53:03 +0000

    Added 145. Binary Tree Postorder Traversal recursive solution in GoLang

---
+++ 145. Binary Tree Postorder Traversal/145-recursive.go
@@ -0,0 +1,34 @@
+// 145. Binary Tree Postorder Traversal recursive solution in GoLang
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+package leetcode
+
+ func postorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+
+    var output []int
+    traverse(root, &output)
+
+    return output
+}
+
+func traverse(root *TreeNode, output *[]int) {
+    if root == nil {
+        return
+    }
+
+    traverse(root.Left, output)
+    traverse(root.Right, output)
+    *output = append(*output, root.Val)
+}
