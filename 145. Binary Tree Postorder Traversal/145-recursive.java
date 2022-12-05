088bc93f4f3470ce7186fffd84b3c9e0aaa0e55b
kushal-kamra <kushalkamradev@gmail.com>
2022-12-05 15:02:30 +0000

    Added 145. Binary Tree Postorder Traversal recursive solution in Java

---
+++ 145. Binary Tree Postorder Traversal/145-recursive.java
@@ -0,0 +1,31 @@
+// 145. Binary Tree Postorder Traversal recursive solution in Java
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        if (root != null) {
+            result.addAll(postorderTraversal(root.left));
+            result.addAll(postorderTraversal(root.right));
+            result.add(root.val);
+        }
+
+        return result;
+    }
+}
