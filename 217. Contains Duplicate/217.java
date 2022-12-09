bdf1d74c84114dcef20b51da934cecc0b28010d4
kushal-kamra <kushalkamra@gmail.com>
2022-12-09 11:59:55 +0530

    Added 217. Contains Duplicate solution in Java

---
+++ 217. Contains Duplicate/217.java
@@ -0,0 +1,16 @@
+// 217. Contains Duplicate solution in Java
+// https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+
+        for (int x : nums) {
+            if (set.contains(x)) return true;
+
+            set.add(x);
+        }
+
+        return false;
+    }
+}
