bb699d2c48ca67186fa70b41c491072aa68e5ef2
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 05:57:12 +0000

    Added 704. Binary Search solution in JavaScript

---
+++ 704. Binary Search/704.js
@@ -0,0 +1,27 @@
+// 704. Binary Search solution in JavaScript
+// https://leetcode.com/problems/binary-search/description/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let low = 0, high = nums.length - 1;
+
+    while (low < high) {
+        let mid = low + Math.floor((high - low) / 2);
+
+        console.log('mid : ', mid);
+
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return nums[low] == target ? low : -1;
+};
