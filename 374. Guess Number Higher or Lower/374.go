268c0662ee98e4b3642f1e0b114ca59c46e4f1ae
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 08:13:36 +0000

    Added 374. Guess Number Higher or Lower solution in GoLang

---
+++ 374. Guess Number Higher or Lower/374.go
@@ -0,0 +1,34 @@
+// 374. Guess Number Higher or Lower solution in GoLang
+// https://leetcode.com/problems/guess-number-higher-or-lower/description/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+ package leetcode
+
+ func guessNumber(n int) int {
+    low := 1
+    high := n
+
+    for ;low <= high; {
+        mid := low + ((high - low) / 2)
+
+        guess_res := guess(mid)
+
+        if guess_res == 0 {
+            return mid
+        } else if guess_res == 1 {
+            low = mid + 1
+        } else if guess_res == -1 {
+            high = mid - 1
+        }
+    }
+
+    return -1
+}
