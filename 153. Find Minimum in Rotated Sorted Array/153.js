95738bd865c17d3ea23d767cf57b0f07f62af03c
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 12:17:33 +0000

    Added 153. Find Minimum in Rotated Sorted Array

---
+++ 153. Find Minimum in Rotated Sorted Array/153.js
@@ -0,0 +1,23 @@
+// 153. Find Minimum in Rotated Sorted Array
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+};
