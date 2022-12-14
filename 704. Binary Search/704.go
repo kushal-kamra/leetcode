202ae4afe94545edbf849819091656540defae1f
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 06:08:59 +0000

    Added 704. Binary Search solution in GoLang

---
+++ 704. Binary Search/704.go
@@ -0,0 +1,25 @@
+// 704. Binary Search solution in GoLang
+// https://leetcode.com/problems/binary-search/description/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    let low = 0, high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return nums[low] == target ? low : -1;
+};
