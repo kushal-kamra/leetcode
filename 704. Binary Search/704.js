4c6e035ba31bd06dff49f083742a17002a9a03e1
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 06:08:18 +0000

    704 - Altering while

--- 704. Binary Search/704.js
+++ 704. Binary Search/704.js
@@ -9,7 +9,7 @@
 var search = function(nums, target) {
     let low = 0, high = nums.length - 1;
 
-    while (low < high) {
+    while (low <= high) {
         let mid = low + Math.floor((high - low) / 2);
 
         if (target == nums[mid]) {
