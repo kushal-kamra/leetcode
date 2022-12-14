9f15b4d9f92d6b27bb0d869f29cd1016d830cce6
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 06:09:45 +0000

    704 - Correcting go code

--- 704. Binary Search/704.go
+++ 704. Binary Search/704.go
@@ -1,25 +1,23 @@
 // 704. Binary Search solution in GoLang
 // https://leetcode.com/problems/binary-search/description/
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
- var search = function(nums, target) {
-    let low = 0, high = nums.length - 1;
+package leetcode
 
-    while (low <= high) {
-        let mid = low + Math.floor((high - low) / 2);
+func search(nums []int, target int) int {
+    low := 0
+    high := len(nums) - 1
 
-        if (target == nums[mid]) {
-            return mid;
-        } else if (target < nums[mid]) {
-            high = mid - 1;
+    for ;low <= high; {
+        mid := low + (high - low) / 2
+
+        if target == nums[mid] {
+            return mid
+        } else if target < nums[mid] {
+            high = mid - 1
         } else {
-            low = mid + 1;
+            low = mid + 1
         }
     }
 
-    return nums[low] == target ? low : -1;
-};
+    return -1
+}
