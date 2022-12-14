46f448a7e51a6124478fa1cbad4218bf998c39c7
kushal-kamra <kushalkamradev@gmail.com>
2022-12-14 09:45:24 +0000

    Added 278. First Bad Version solution in GoLang

---
+++ 278. First Bad Version/278.go
@@ -0,0 +1,29 @@
+// 278. First Bad Version solution in GoLang
+// https://leetcode.com/problems/first-bad-version/description/
+
+/** 
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad 
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+package leetcode
+
+ func firstBadVersion(n int) int {
+    left := 1
+    right := n
+
+    for left < right {
+        mid := left + ((right - left) / 2)
+
+        if isBadVersion(mid) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return left
+}
