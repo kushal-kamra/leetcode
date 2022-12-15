35610d646682401398c1368c84f6d86ee658c92a
kushal-kamra <kushalkamradev@gmail.com>
2022-12-15 16:09:52 +0000

    Added 702. Search in a Sorted Array of Unknown Size sokution in GoLang

---
+++ 702. Search in a Sorted Array of Unknown Size/702.go
@@ -0,0 +1,40 @@
+// 702. Search in a Sorted Array of Unknown Size sokution in GoLang
+// https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/description/
+
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * type ArrayReader struct {
+ * }
+ *
+ * func (this *ArrayReader) get(index int) int {}
+ */
+
+ func search(reader ArrayReader, target int) int {
+    if reader.get(0) == target {
+        return 0
+    }
+
+    left := 0
+    right := 1
+
+    for reader.get(right) < target {
+        left = right
+        right = right * 2
+    }
+
+    for left <= right {
+        mid := left + (right - left) / 2
+        mid_res := reader.get(mid)
+
+        if mid_res == target {
+            return mid
+        } else if target < mid_res {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+
+    return -1
+}
