b6f4da5daf9b2f2912ee00bd3fdda33d49aeeab0
kushal-kamra <kushalkamradev@gmail.com>
2022-12-16 04:46:33 +0000

    Added 367. Valid Perfect Square solution in GoLang

---
+++ 367. Valid Perfect Square/367.go
@@ -0,0 +1,31 @@
+// 367. Valid Perfect Square solution in GoLang
+// https://leetcode.com/problems/valid-perfect-square/description/
+
+package leetcode
+
+func isPerfectSquare(num int) bool {
+    if num < 2 {
+        return true
+    }
+
+    left := 2
+    right := num / 2
+
+    var pivot int
+
+    for ;left <= right; {
+        pivot = left + ((right - left) / 2)
+
+        pivot_square := pivot * pivot
+
+        if pivot_square == num {
+            return true
+        } else if pivot_square > num {
+            right = pivot - 1
+        } else if pivot_square < num {
+            left = pivot + 1
+        }
+    }
+
+    return false
+}
