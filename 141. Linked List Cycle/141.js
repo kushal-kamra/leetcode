a6e775330e4fd3a98fdd834d0d6ef7b9700755bd
kushal-kamra <kushalkamra@gmail.com>
2022-12-19 18:18:06 +0530

    Added 141. Linked List Cycle solution in JavaScript

---
+++ 141. Linked List Cycle/141.js
@@ -0,0 +1,30 @@
+// 141. Linked List Cycle solution in JavaScript
+// https://leetcode.com/problems/linked-list-cycle/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (head == null) return false;
+
+    let slow = head;
+    let fast = head.next;
+
+    while (slow != fast) {
+        if (fast == null || fast.next == null) return false;
+
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return true;
+};
