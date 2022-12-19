eacbc448f20d51182e0407fe7b3c393cee0d2c25
kushal-kamra <kushalkamra@gmail.com>
2022-12-19 18:07:33 +0530

    Added 142. Linked List Cycle II solution in GoLang

---
+++ 142. Linked List Cycle II/142.go
@@ -0,0 +1,48 @@
+// 142. Linked List Cycle II solution in GoLang
+// https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func detectCycle(head *ListNode) *ListNode {
+    if head == nil {
+        return head
+    }
+
+    intersection_node := getIntersection(head)
+    if intersection_node == nil {
+        return nil
+    }
+
+    ptr1 := head
+    ptr2 := intersection_node
+
+    for ptr1 != ptr2 {
+        ptr1 = ptr1.Next
+        ptr2 = ptr2.Next
+    }
+
+    return ptr1
+}
+
+func getIntersection(head *ListNode) *ListNode {
+    slow := head
+    fast := head
+
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+
+        if fast == slow {
+            return slow
+        }
+    }
+
+    return nil
+}
