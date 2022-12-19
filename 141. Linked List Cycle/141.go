6fb4406f0a7611d987358486f8d6e21e23349e16
kushal-kamra <kushalkamra@gmail.com>
2022-12-19 18:20:17 +0530

    Added 141. Linked List Cycle solution in GoLang

---
+++ 141. Linked List Cycle/141.go
@@ -0,0 +1,29 @@
+// 141. Linked List Cycle solution in GoLang
+// https://leetcode.com/problems/linked-list-cycle/description/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func hasCycle(head *ListNode) bool {
+    if head == nil {
+        return false
+    }
+
+    slow := head
+    fast := head.Next
+
+    for slow != fast {
+        if fast == nil || fast.Next == nil {
+            return false
+        }
+
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+
+    return true
+}
