86788bd1fbd694cecb3c99018980558df482886c
kushal-kamra <kushalkamradev@gmail.com>
2023-03-20 22:07:44 +0530

    Added 1757. Recyclable and Low Fat Products SQL

---
+++ 1757. Recyclable and Low Fat Products/1757.sql
@@ -0,0 +1,4 @@
+-- 1757. Recyclable and Low Fat Products
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+select product_id from products where low_fats = 'Y' and recyclable = 'Y';
