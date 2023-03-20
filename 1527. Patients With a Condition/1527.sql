d11a0ad9752ae5f2e512e2dfa9bd85b652e281c3
kushal-kamra <kushalkamradev@gmail.com>
2023-03-20 22:18:38 +0530

    Added 1527. Patients With a Condition SQL

---
+++ 1527. Patients With a Condition/1527.sql
@@ -0,0 +1,6 @@
+-- 1527. Patients With a Condition
+-- https://leetcode.com/problems/patients-with-a-condition/
+
+SELECT * FROM PATIENTS WHERE
+CONDITIONS LIKE '% DIAB1%' OR
+CONDITIONS LIKE 'DIAB1%';
